28adbc38c06ca525e0365722c6d219022e3214b7
SamiumBashir <samiumbashirbosunia@gmail.com>
2026-04-08 04:41:56 +0600

    bus fare calc

--- JS-conditionals-tasks/6 bus fare calculator.js
+++ JS-conditionals-tasks/6 bus fare calculator.js
@@ -6,3 +6,23 @@ Ticket fare Calculator
     - Senior citizens (age >= 60) gets a 15% Discount
     - Otherwise Regular ticket fare 800 tk
 */
+
+const age = 9;
+const isStudent =true;
+const ticketFare = 800;
+
+if(age < 10){
+    console.log ("Free");
+}
+else if ( isStudent == true){
+    studentFare = ticketFare / 2;
+    console.log("Student Ticket Fare:" ,studentFare);
+}
+else if (age >=60) {
+    discountFare = ticketFare * 15/100;
+    seniorCitizenFare = ticketFare - discountFare;
+    console.log ("Senior Citizen Fare:", seniorCitizenFare);
+}
+else{
+    console.log ("Regular Ticket Fare", ticketFare)
+}
