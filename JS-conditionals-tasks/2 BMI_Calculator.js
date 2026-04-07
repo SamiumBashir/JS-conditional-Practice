a3994315b4054b3ffee411cbb78f52c2635d0984
SamiumBashir <samiumbashirbosunia@gmail.com>
2026-04-07 23:53:58 +0600

    BMI calc

--- JS-conditionals-tasks/2 BMI_Calculator.js
+++ JS-conditionals-tasks/2 BMI_Calculator.js
@@ -11,3 +11,28 @@ Create a JavaScript program that calculates the Body Mass Index (BMI) and assign
     - Otherwise, you are obese.
 
 */
+
+const weight = 72;
+const heightInFeet = 5.6;
+
+const heightInMeter = heightInFeet * 0.3048;
+
+const bmi = weight / (heightInMeter * heightInMeter);
+
+console.log ("Your BMI is " +bmi.toFixed(2));
+
+if (bmi < 18.5) {
+    console.log ("you are underweight");
+}
+
+else if (bmi >= 18.5 && bmi <= 24.9){
+    console.log ("you are normal");
+}
+
+else if (bmi >= 25 && bmi <= 29.9) {
+    console.log ("you are overweight");
+}
+
+else {
+    console.log ("you are obese");
+}
