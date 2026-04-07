71fc2d1c7ce3f20445e5359d395fe3255ae0bec5
SamiumBashir <samiumbashirbosunia@gmail.com>
2026-04-07 23:37:18 +0600

    free drinks

---
+++ JS-conditionals-tasks/1 Free Drinks.js
@@ -0,0 +1,16 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+
+const burgerPrice = 400;
+
+if (burgerPrice > 500) {
+    console.log("free coke");
+}
+else {
+    console.log("coke : 30tk");
+}
