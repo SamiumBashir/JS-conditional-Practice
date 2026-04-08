e307fec6eaa1b8a86543b0ba4804b135e917c91d
SamiumBashir <samiumbashirbosunia@gmail.com>
2026-04-08 19:57:58 +0600

    odd-even js

--- js-loop-tasks/while/2 odd even.js
+++ js-loop-tasks/while/2 odd even.js
@@ -16,3 +16,20 @@ Find all the even numbers from 78 to 98.
  */
 
 /*programming hero*/
+
+let oddNumber = [];
+let i = 61;
+while (i < 100) {
+    oddNumber.push(i);
+    i+=2;
+}
+console.log("Odd Number List",oddNumber);
+
+let evenNumberContainer = [];
+let evenNumber = 78;
+
+while (evenNumber < 100) {
+    evenNumberContainer.push(evenNumber);
+    evenNumber +=2;
+}
+console.log("Even Number List",evenNumberContainer);
