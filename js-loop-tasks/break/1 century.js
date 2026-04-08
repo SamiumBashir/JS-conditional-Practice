42187e6c94f7e19548c5daad4f2659062df36836
SamiumBashir <samiumbashirbosunia@gmail.com>
2026-04-08 16:29:55 +0600

    w-commitment.js

---
+++ js-loop-tasks/break/1 century.js
@@ -0,0 +1,6 @@
+/* 
+
+Write a loop 1 to 200. Use break to exit the loop once you find 100.
+
+*/
+
---
+++ js-loop-tasks/while/1 commitment.js
@@ -0,0 +1,14 @@
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+
+/*programming hero*/
+
+let messageCount = 0;
+
+while (messageCount <= 60 ) {
+    console.log ("I will invest at least 6 hours every single day for 60 days!");
+    messageCount++;
+}
