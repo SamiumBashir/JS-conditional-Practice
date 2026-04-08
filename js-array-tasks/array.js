25225f8da8585d2a8332468398b427e66321ed5f
SamiumBashir <samiumbashirbosunia@gmail.com>
2026-04-08 14:54:34 +0600

    concat methods

---
+++ js-array-tasks/array.js
@@ -0,0 +1,10 @@
+const phones = ['apple', 'samsung', 'One plus', 'Infinity'];
+const names = ('apple', 'samsung', 'oneplus', 'vivo');
+
+if (Array.isArray (names)){
+    console.log("lets buy a phone")
+}
+
+else{
+    console.log("onno dokan dekhi")
+}
---
+++ js-array-tasks/combain.js
@@ -0,0 +1,8 @@
+const fruits = ['banana', 'lichi', 'mango', 'orange'];
+const sports = ['cricket', 'football', 'hockey', 'hadudu'];
+
+myChoice = fruits.concat(sports);
+
+console.log(fruits);
+console.log(sports);
+console.log(myChoice);
