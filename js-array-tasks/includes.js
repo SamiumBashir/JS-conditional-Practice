33d40bf071702b2d8f62dc45d2501eaa3da6dea1
SamiumBashir <samiumbashirbosunia@gmail.com>
2026-04-08 13:38:33 +0600

    includes

---
+++ js-array-tasks/includes.js
@@ -0,0 +1,11 @@
+const bookList = ['Himu', 'ordhanggini', 'ordhek nari ordhek isshori', 'gavi brittanto']
+
+findBook = bookList.includes('javascript')
+
+if (findBook == true) {
+    console.log('Lets Practice')
+}
+
+else{
+    console.log ('Find the other One')
+}
