9ae3724f12fb8a6d9cab223941e653edd235310b
SamiumBashir <samiumbashirbosunia@gmail.com>
2026-04-09 01:31:45 +0600

    sum of even numbers

--- js-loop-tasks/for/3 sum of odd numbers.js
+++ js-loop-tasks/for/3 sum of odd numbers.js
@@ -16,3 +16,31 @@ Display sum of all the even numbers from 51 to 85.
  */
 
 /*programming hero*/
+
+
+let oddNumberList = []; //declaration 
+let totalSum = 0; // declaration 
+
+for( oddNumber = 91 ; oddNumber <= 129; oddNumber++){
+    if (oddNumber % 2 === 1){
+        oddNumberList.push(oddNumber);
+        totalSum += oddNumber;
+    }
+}
+
+console.log("Total Sum", totalSum);
+console.log("Odd Number", oddNumberList);
+
+
+let evenNumberList = [];
+let totalOfEvenNumber = 0;
+
+for (evenNumber = 51; evenNumber <= 85; evenNumber++){
+    if (evenNumber % 2 === 0){
+        evenNumberList.push(evenNumber);
+        totalOfEvenNumber += evenNumber;
+    }
+}
+
+console.log("List", evenNumberList);
+console.log ("Even Numbers Sum", totalOfEvenNumber);
