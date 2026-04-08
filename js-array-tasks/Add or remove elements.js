a073dad05e9392714ed20caea401cccfc74bb0f4
SamiumBashir <samiumbashirbosunia@gmail.com>
2026-04-08 13:19:07 +0600

    Add or remove elements

---
+++ js-array-tasks/Add or remove elements.js
@@ -0,0 +1,6 @@
+const tourSpot = ['Bandorban', 'Rajshahi', 'pahartoli'];
+
+tourSpot.push('Chilahati');
+tourSpot.push('Naoga', 'Chadpur')
+tourSpot.pop('')
+console.log(tourSpot)
--- js-array-tasks/Declare an array .js
+++ js-array-tasks/Declare an array .js
@@ -1,3 +1,7 @@
+// index = n-1;[n = element of array];
+
+
+
 const nameFruit = ["banana", "lichi", "mango", "apple", "orange"];
 console.log (nameFruit[3]);
 nameFruit[2] = "jambura";
