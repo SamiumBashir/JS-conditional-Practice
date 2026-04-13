efd9641732ffe6cede484a5f41f8f4f2e8ed9c6a
SamiumBashir <samiumbashirbosunia@gmail.com>
2026-04-14 03:19:57 +0600

    practicing on DOM

--- DOM Practice/practice-1.js
+++ DOM Practice/practice-1.js
@@ -1,2 +1,37 @@
 const allHeading = document.getElementsByTagName('h1');
 console.log (allHeading);
+for(let headings of allHeading){
+    headings.innerText = 'Atao naki heading';
+    headings.innerText = headings.innerText.replace('Atao', 'Otao')
+    console.log(headings.innerText);
+}
+
+const listItems = document.getElementById('list');
+console.log (listItems);
+
+const items = document.querySelectorAll('#list li')
+console.log( items[1]);
+
+items[1].innerText = 'ami valo achi';
+items[1].style.color = 'red';
+items[1].style.backgroundColor = 'black'
+
+//parent element 
+
+const section = document.createElement('section');
+
+
+// child element 
+
+const h1 = document.createElement('h1');
+h1.innerText = 'Hola Hola Shakib'
+const p = document.createElement(p)
+p
+
+// append to parent elemet ;
+
+
+section.appendChild(h1)
+document.body.appendChild(section);
+
+
