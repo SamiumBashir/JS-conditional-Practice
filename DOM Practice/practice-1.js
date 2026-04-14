cbb3490b04789372398eaa58463ff2342d983845
SamiumBashir <samiumbashirbosunia@gmail.com>
2026-04-14 20:34:38 +0600

    practice on DOM

--- DOM Practice/practice-1.js
+++ DOM Practice/practice-1.js
@@ -20,18 +20,44 @@ items[1].style.backgroundColor = 'black'
 
 const section = document.createElement('section');
 
+// loop দিয়ে multiple card বানাও
+for (let i = 1; i <= 3; i++) {
+  const card = document.createElement('div');
 
-// child element 
+  // child element
+  const h1 = document.createElement('h1');
+  h1.innerText = `Card Title ${i}`;
 
-const h1 = document.createElement('h1');
-h1.innerText = 'Hola Hola Shakib'
-const p = document.createElement(p)
-p
+  const p = document.createElement('p');
+  p.innerText = `This is card number ${i}`;
 
-// append to parent elemet ;
+  // append
+  card.appendChild(h1);
+  card.appendChild(p);
 
+  // style (optional)
+  card.style.backgroundColor = '#fff';
+  card.style.margin = '10px 0';
+  card.style.padding = '10px';
+  card.style.borderRadius = '8px';
 
-section.appendChild(h1)
+  // section এ add
+  section.appendChild(card);
+}
+
+// শেষে body তে add
 document.body.appendChild(section);
+
+// section style
+section.style.display = 'flex';
+section.style.justifyContent = 'space-around'
+section.style.gap = '20px'
+section.style.maxWidth = '1140px';
+section.style.backgroundColor = '#aaa';
+section.style.padding = '15px';
+section.style.margin = '0 150px';
+section.style.border = '2px solid green';
+section.style.borderRadius = '12px';
+
 
 
