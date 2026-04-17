1654e5f6f6e9c30959cfcb7c332af98bd71c007a
SamiumBashir <samiumbashirbosunia@gmail.com>
2026-04-17 23:54:30 +0600

    practice js

---
+++ javascript Basic/test-2.js
@@ -0,0 +1,11 @@
+let hour = 15;
+
+if(hour >= 6 && hour < 12){
+    console.log('Good Morning');
+}
+else if(hour >= 18 && hour < 24){
+    console.log('Good Afternoon')
+}
+else{
+    console.log('Good Evening');
+}
---
+++ javascript Basic/test-3.js
@@ -0,0 +1,13 @@
+
+
+function max (numbs1, numbs2){
+    if(numbs1 > numbs2){
+        return numbs1
+    }
+    else{
+        return numbs2
+    }
+}
+
+let maxNumbs = max(4, 4);
+console.log(maxNumbs);
